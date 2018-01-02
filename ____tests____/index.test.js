08f0bbdaeb81c1ce61e8b6b2123d6275153412ad
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-02 14:44:17 +0800

    test

--- ____tests____/index.test.js
+++ ____tests____/index.test.js
@@ -2,8 +2,7 @@ const execa = require('execa');
 
 test('Show random hitokoto', done => {
 	execa('./cli.js').then(result => {
-		console.log(result.stdout);
-		expect(result.stdout.charAt(0)).toBe('✔');
+		expect(typeof result.stdout).toBe('string');
 		done();
 	});
 });
